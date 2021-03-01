f6987851952ef88cbe5c1c136f6924a6e3bca6af
Marcos André <=>
2021-03-01 16:10:42 -0300

    client side feito, criar novas funcionalidades e serviços

--- PaymentBackend/PaymentApi/Startup.cs
+++ PaymentBackend/PaymentApi/Startup.cs
@@ -29,11 +29,17 @@ namespace PaymentApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentApi", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
